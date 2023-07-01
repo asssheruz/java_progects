aab961ef4a29e94fc71994bb98b8b712666a2dd3
Turaev Sherzod Gayratovich <asssher.uz@gmail.com>
2023-07-01 19:14:54 +0500

    Тип данных

--- program.java
+++ program.java
@@ -4,6 +4,15 @@
 public class program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		int a = 13;
+		short age = 10;
+		double pi = 3.1415;
+		char ch = '1';
+		float f = 45.25f;
+		System.out.println(a);
+		System.out.println(age);
+		System.out.println(pi);
+		System.out.println(ch);
+		System.out.println(f);
 	}
 }
